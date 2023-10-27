4fcc0ce20a09c8fdb1e4d97a71fdeb36d540e70f
omarjafor <omarjaforchy@gmail.com>
2023-10-27 19:54:47 +0600

    delete method applied

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingsCollection.deleteOne(query);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
